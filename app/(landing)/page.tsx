cf47f4270bcdc4e06e07c4d05d57c5c5785b7332
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-01-28 22:01:30 +0530

    created landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,15 @@
-import { Button } from "@/components/ui/button";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
-
-export default function LandingPage() {
-  return (
-    <Button>Landing Page</Button>
-  );
+const LandingPage = () => {
+  return ( 
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+   );
 }
+ 
+export default LandingPage;
