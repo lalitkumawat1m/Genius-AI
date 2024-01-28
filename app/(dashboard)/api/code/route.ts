fe4f05dea721b4be6e5a6ea27c656ab382bbfde8
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-01-28 19:43:56 +0530

    open ai api giving ratelimiting error

---
+++ app/(dashboard)/api/code/route.ts
@@ -0,0 +1,57 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import  OpenAI from "openai";
+
+// import { checkSubscription } from "@/lib/subscription";
+// import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    maxRetries:10
+});
+
+const instructionMessage: ChatCompletionMessageParam = {
+  role: "system",
+  content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
+};
+
+export async function POST(
+  req: Request
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages  } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    // const freeTrial = await checkApiLimit();
+    // const isPro = await checkSubscription();
+
+    // if (!freeTrial && !isPro) {
+    //   return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+    // }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages]
+    });
+
+    // if (!isPro) {
+    //   await incrementApiLimit();
+    // }
+    console.log(response.choices[0].message.content);
+    return NextResponse.json(response.choices[0].message.content);
+  } catch (error) {
+    console.log('[CODE_ERROR]', error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function LandingPage() {
+  return (
+    <Button>Landing Page</Button>
+  );
+}
